276cf96a4728132303a57008adf51e737092f5ea
electricBeard <94660478+electricBeard@users.noreply.github.com>
2023-03-30 21:22:36 -0700

    template out project 2 area

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,13 +1,19 @@
 "use strict";
 
-let slideIndex = [1,1,1,1];
+let slideIndex = [1,1,1,1,1,1,1,1];
 // Class the members of each slideshow group with different CSS classes
 
-let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
-showSlides(1, 0);
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4",
+    "mySlides5", "mySlides6", "mySlides7", "mySlides8"]
+
+showSlides(1,0);   
 showSlides(1,1);
 showSlides(1,2);
 showSlides(1,3);
+showSlides(1,4);   
+showSlides(1,5);
+showSlides(1,6);
+showSlides(1,7);
 
 function plusSlides(n, no){
     showSlides(slideIndex[no] += n, no);
